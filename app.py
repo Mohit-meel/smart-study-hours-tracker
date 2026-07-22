3e6d84b6cf973275d8cdb204cb4a177fcca386b5
Mohit Meel <mohitmeel2518@gmail.com>
2026-07-22 21:13:48 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from db import create_table, insert_record, view_records
+from db import (
+    create_table,
+    insert_record,
+    view_records,
+    update_record,
+    delete_record,
+    search_record
+)
 
 def main():
     create_table()
@@ -9,7 +16,10 @@ def main():
         print("====================================")
         print("1. Add Study Record")
         print("2. View Study Records")
-        print("3. Exit")
+        print("3. Update Study Record")
+        print("4. Delete Study Record")
+        print("5. Search Study Record")
+        print("6. Exit")
 
         choice = input("\nEnter your choice: ")
 
@@ -42,6 +52,44 @@ def main():
                 print("-----------------------------------")
 
         elif choice == "3":
+            record_id = int(input("Enter Record ID to update: "))
+            name = input("Enter new name: ")
+            subject = input("Enter new subject: ")
+            hours = float(input("Enter new study hours: "))
+            date = input("Enter new date (YYYY-MM-DD): ")
+
+            update_record(record_id, name, subject, hours, date)
+
+            print("\nRecord updated successfully!")
+
+        elif choice == "4":
+            record_id = int(input("Enter Record ID to delete: "))
+
+            delete_record(record_id)
+
+            print("\nRecord deleted successfully!")
+
+        elif choice == "5":
+            name = input("Enter student name: ")
+
+            records = search_record(name)
+
+            if len(records) == 0:
+                print("\nNo records found.")
+
+            else:
+                print("\n========== Search Results ==========")
+
+                for record in records:
+                    print("-----------------------------------")
+                    print(f"ID      : {record[0]}")
+                    print(f"Name    : {record[1]}")
+                    print(f"Subject : {record[2]}")
+                    print(f"Hours   : {record[3]}")
+                    print(f"Date    : {record[4]}")
+                print("-----------------------------------")
+
+        elif choice == "6":
             print("\nThank you for using Smart Study Hours Tracker!")
             break
 
--- db.py
+++ db.py
@@ -1,18 +1,107 @@
 import psycopg2
 
+
 def get_connection():
     connection = psycopg2.connect(
         host="localhost",
         database="study_tracker",
         user="postgres",
-        password="Mohit@Mk"
+        password="501050@Mk"
     )
     return connection
 
-if __name__ == "__main__":
-    try:
-        conn = get_connection()
-        print("Connected to PostgreSQL successfully!")
-        conn.close()
-    except Exception as e:
-        print("Error:", e)
+
+def create_table():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS study_records(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        subject VARCHAR(100),
+        hours FLOAT,
+        date DATE
+    )
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def insert_record(name, subject, hours, date):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO study_records(name, subject, hours, date) VALUES (%s, %s, %s, %s)",
+        (name, subject, hours, date)
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def view_records():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM study_records ORDER BY id")
+
+    records = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return records
+
+
+def update_record(record_id, name, subject, hours, date):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        UPDATE study_records
+        SET name=%s,
+            subject=%s,
+            hours=%s,
+            date=%s
+        WHERE id=%s
+    """, (name, subject, hours, date, record_id))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def delete_record(record_id):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "DELETE FROM study_records WHERE id=%s",
+        (record_id,)
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def search_record(name):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM study_records WHERE name=%s",
+        (name,)
+    )
+
+    records = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return records
